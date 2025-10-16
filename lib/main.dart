cd8f42c1c0f1ce56cb79f74176d90465d540a853
natthayasongsura <156439856+natthayasongsura@users.noreply.github.com>
2025-10-16 11:42:30 +0700

    update: changes

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('หวัดดี'),
+            const Text('นี่คือโค้ดของนัทยา'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
